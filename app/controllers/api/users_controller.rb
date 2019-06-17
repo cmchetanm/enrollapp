651881e753a3dce890b81fc81dd6bffe066ba226
Vadim Kononov <vadim@poetic.com>
2019-06-17 07:11:58 -0500

    Added a more robust method of determining client id for saving fcm token. Added missing email assets

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,13 +1,22 @@
 module Api
   class UsersController < ApplicationController
+    before_action :set_client
+
     def fcm_token
-      current_api_user.fcm_tokens[request.headers['client']] = params[:token]
+      current_api_user.fcm_tokens[@client_id] = params[:token]
 
       if current_api_user.save
         head :created
       else
         render json: {errors: current_api_user.errors.full_messages}, status: :unprocessable_entity
       end
+    end
+
+    private
+
+    def set_client
+      client_name = DeviseTokenAuth.headers_names[:client]
+      @client_id = request.headers[client_name] || params[client_name]
     end
   end
 end
