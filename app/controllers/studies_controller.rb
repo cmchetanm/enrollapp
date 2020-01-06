90ac27561364985deee645e196516fb0d9df0a24
Contrarian Coding <contrariancoding@gmail.com>
2020-01-06 02:57:12 +0200

    Trying to fix bug
    notice to puts
    And image tag to div

--- app/controllers/studies_controller.rb
+++ app/controllers/studies_controller.rb
@@ -2,25 +2,25 @@ class StudiesController < ApplicationController
   before_action :set_study, only: %i[show edit update destroy]
 
   def index
-    notice 'index'
+    puts 'index'
     @studies = Study.all.order(:name)
   end
 
   def show
-    notice 'show'
+    puts 'show'
   end
 
   def new
-    notice 'new'
+    puts 'new'
     @study = Study.new(topic: Topic.find_by(id: params[:topic_id]))
   end
 
   def edit
-    notice 'edit'
+    puts 'edit'
   end
 
   def create
-    notice 'create'
+    puts 'create'
     @study = Study.new(study_params)
 
     if @study.save
@@ -31,7 +31,7 @@ class StudiesController < ApplicationController
   end
 
   def update
-    notice 'update'
+    puts 'update'
     if @study.update(study_params)
       redirect_to @study, notice: 'Study was successfully updated.'
     else
@@ -40,7 +40,7 @@ class StudiesController < ApplicationController
   end
 
   def destroy
-    notice 'destroy'
+    puts 'destroy'
     if @study.destroy
       redirect_to studies_url, notice: 'Study was successfully destroyed.'
     else
@@ -51,12 +51,12 @@ class StudiesController < ApplicationController
   private
 
   def set_study
-    notice 'set_study'
+    puts 'set_study'
     @study = Study.find(params[:id])
   end
 
   def study_params
-    notice 'study_params'
+    puts 'study_params'
     params.require(:study).permit(
       :topic_id, :name, :study_icon, :protocol, :agent, :mechanism, :side_effects, :administration, :randomization,
       :duration, :assessment_frequency, :interventions, :sponsor_name, :sponsor_contact, :cro_contact,
