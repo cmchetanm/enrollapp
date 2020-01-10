01ba8e995f7070cc8048c18dd0be215e7e1aa621
Contrarian Coding <contrariancoding@gmail.com>
2020-01-10 04:19:09 +0200

    Add api to specific check route
    Should be in the namespace so probably a different problem

--- config/routes.rb
+++ config/routes.rb
@@ -40,7 +40,7 @@ Rails.application.routes.draw do
     resources :messages, except: %i[new edit]
     resources :users
     put '/auth/fcm_token', to: 'users#fcm_token'
-    put '/auth/check_exists', to: 'users#check_exists'
+    put '/api/auth/check_exists', to: 'users#check_exists'
   end
 
   match '/401', to: 'errors#unauthorized', via: :all, as: :unauthorized
