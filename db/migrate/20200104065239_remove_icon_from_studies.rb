2dda8b37294933350eb29c88b4de95a50529f122
Contrarian Coding <contrariancoding@gmail.com>
2020-01-14 05:52:30 +0200

    Restore old server return
    To check list in normal app

--- db/migrate/20200104065239_remove_icon_from_studies.rb
+++
@@ -1,5 +0,0 @@
-class RemoveIconFromStudies < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :studies, :study_icon, :string
-  end
-end
--- db/migrate/20200104065240_remove_icon_from_study_versions.rb
+++
@@ -1,5 +0,0 @@
-class RemoveIconFromStudyVersions < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :study_versions, :study_icon, :string
-  end
-end
