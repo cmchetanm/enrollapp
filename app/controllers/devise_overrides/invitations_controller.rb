bac4e267b865d7daebad03151f178ddd04e293a2
Contrarian Coding <contrariancoding@gmail.com>
2020-01-16 22:33:26 +0200

    invitation controller prints
    show vars on creater in invitation controller

--- app/controllers/devise_overrides/invitations_controller.rb
+++ app/controllers/devise_overrides/invitations_controller.rb
@@ -2,6 +2,33 @@ module DeviseOverrides
   class InvitationsController < Devise::InvitationsController
     before_action :configure_permitted_parameters, if: :devise_controller?
 
+    # POST /resource/invitation
+    def create
+      self.resource = invite_resource
+      resource_invited = resource.errors.empty?
+
+      yield resource if block_given?
+      puts 'invite_resource'
+      puts invite_resource
+      puts 'is_flashing_format?'
+      puts is_flashing_format?
+      puts 'is_flashing_format'
+      puts is_flashing_format
+
+      if resource_invited
+        if is_flashing_format? && self.resource.invitation_sent_at
+          set_flash_message :notice, :send_instructions, email: self.resource.email
+        end
+        if self.method(:after_invite_path_for).arity == 1
+          respond_with resource, location: after_invite_path_for(current_inviter)
+        else
+          respond_with resource, location: after_invite_path_for(current_inviter, resource)
+        end
+      else
+        respond_with_navigational(resource) { render :new }
+      end
+    end
+
     protected
 
     def after_invite_path_for(_inviter, _invitee = nil)
