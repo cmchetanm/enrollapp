5b5b303ef0e4edead69682ae8fa777db00f5ceea
Contrarian Coding <contrariancoding@gmail.com>
2020-01-16 22:58:07 +0200

    Revert "Last prints"
    This reverts commit 13a7e22911f7e9c748d8c3f2ca07c29c63af7cca.

--- app/controllers/devise_overrides/registrations_controller.rb
+++ app/controllers/devise_overrides/registrations_controller.rb
@@ -4,24 +4,6 @@ module DeviseOverrides
   class RegistrationsController < Devise::RegistrationsController
     prepend_before_action :authenticate_scope!, unless: :no_resources?
 
-    #GET /resource/sign_in
-    def new
-      puts 'new'
-      super
-    end
-
-    #POST /resource/sign_in
-    def create
-      puts 'create'
-      super
-    end
-
-    #DELETE /resource/sign_out
-    def destroy
-      puts 'destroy'
-      super
-    end
-
     private
 
     def no_resources?
