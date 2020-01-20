10ebe67bc2a08ab7fc0c898c3dba1fdea69091a3
Contrarian Coding <contrariancoding@gmail.com>
2020-01-20 03:05:11 +0200

    Print share class
    Why is this a string

--- app/controllers/api/shares_controller.rb
+++ app/controllers/api/shares_controller.rb
@@ -46,6 +46,8 @@ module Api
         params[:shares].each do |share|
           puts '|share|'
           puts share
+          puts 'share.class'
+          puts share.class
           puts 'share.study_id'
           puts share.study_id
           study = StudyAuthenticator.new(current_api_user).find_one(share.study_id)
