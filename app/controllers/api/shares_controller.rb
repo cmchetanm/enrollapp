72e7726395c340ce02f7b5de814a55f1aceabaea
Contrarian Coding <contrariancoding@gmail.com>
2020-01-20 03:26:39 +0200

    One last way
    If this doesn't work idk

--- app/controllers/api/shares_controller.rb
+++ app/controllers/api/shares_controller.rb
@@ -51,8 +51,8 @@ module Api
           share = JSON.parse(sharestring)
           puts 'share'
           puts share
-          puts 'share[:study_id]'
-          puts share[:study_id]
+          puts 'share["study_id"]'
+          puts share["study_id"]
           study = StudyAuthenticator.new(current_api_user).find_one(share[:study_id])
           puts '|study|'
           puts study
