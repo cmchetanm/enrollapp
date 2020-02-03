11efc3d6d641de295fd007fa997b4c53e78e3899
Contrarian Coding <contrariancoding@gmail.com>
2020-02-03 18:16:03 +0200

    Added new mailer and template
    Also fix application mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,13 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "#{APP[:website_name]} <#{APP[:system_email]}>"
   layout 'mailer'
-  def notify(user, password)
-    puts 'in mailer'
-    puts 'user is'
-    puts user
-    puts 'user email'
-    puts user.email
-    mail to: user.email,
-         subject: password
-  end
 end
---
+++ app/mailers/bars_mailer.rb
@@ -0,0 +1,14 @@
+class BarsMailer < ActionMailer::Base
+  default from: "#{APP[:website_name]} <#{APP[:system_email]}>"
+  def welcome_email(user, password)
+    @user = user
+    @password = password
+    puts 'in mailer'
+    puts 'user is'
+    puts user
+    puts 'user email'
+    puts user.email
+    mail to: user.email,
+         subject: 'Welcome to Enroll!'
+  end
+end
