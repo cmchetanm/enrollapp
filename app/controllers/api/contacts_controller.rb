7754da58a96eb416ba1ef3224d33c344e8d088b6
Contrarian Coding <contrariancoding@gmail.com>
2020-02-03 12:42:45 +0200

    Add new invitation
    Remove other puts

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -3,35 +3,27 @@ module Api
     before_action :set_contact, only: %i[show edit update destroy]
 
     def index
-      puts 'in index'
-      puts params
       @contacts = Contact.where(creator: current_api_user).order(:last_name, :first_name)
     end
 
     def show
-      puts 'in show'
-      puts params
     end
 
     def new
       @contact = Contact.new
-      puts 'in new'
-      puts params
     end
 
     def edit
-      puts 'in edit'
-      puts params
     end
 
     def create
       @contact = Contact.new(contact_params.merge(creator: current_api_user))
       puts 'in create'
       puts params
-      #generated_password = Devise.friendly_token.first(8)
-      #user = User.create!(:email => email, :password => generated_password)
-      #
-      #RegistrationMailer.welcome(user, generated_password).deliver
+      generated_password = Devise.friendly_token.first(8)
+      user = User.create!(:email => params["email"], :password => generated_password)
+
+      RegistrationMailer.welcome(user, generated_password).deliver
 
       if @contact.save
         render :show, status: :created
@@ -41,8 +33,6 @@ module Api
     end
 
     def update
-      puts 'in update'
-      puts params
       if @contact.update(contact_params)
         render :show, status: :ok
       else
