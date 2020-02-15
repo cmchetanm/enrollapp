affbd0606158771c2a57b76f4c185f29c088d5f1
Chetan <cmchetanm@gmail.com>
2020-02-15 18:49:20 +0530

    quick fixes

--- app/controllers/api/sites_studies_controller.rb
+++ app/controllers/api/sites_studies_controller.rb
@@ -4,7 +4,7 @@ module Api
     def update_enrolled_committed
       @sites_study = SitesStudy.find_by(site_id: params[:site_id], study_id: params[:study_id])
       if @sites_study.update(enrolled: params[:enrolled], committed: params[:committed])
-        render {enrolled: @sites_study.enrolled, committed: @sites_study.committed}, status: :ok
+        render json: {enrolled: @sites_study.enrolled, committed: @sites_study.committed}, status: :ok
       else
         render json: {errors: @sites_study.errors.full_messages}, status: :unprocessable_entity
       end
