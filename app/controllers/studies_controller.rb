d829d4448aaf6d9eaef94acd8c9b09410ea7d75c
Chetan <cmchetanm@gmail.com>
2020-02-18 14:18:41 +0530

    Fix site's version issue when upadte study from admin portal

--- app/controllers/studies_controller.rb
+++ app/controllers/studies_controller.rb
@@ -27,6 +27,7 @@ class StudiesController < ApplicationController
 
   def update
     if @study.update(study_params)
+      update_study_version
       redirect_to @study, notice: 'Study was successfully updated.'
     else
       render :edit
@@ -45,6 +46,38 @@ class StudiesController < ApplicationController
 
   def set_study
     @study = Study.find params.require(:id)
+  end
+
+  def update_study_version
+    study_versions = @study.study_versions
+    if study_versions.present?
+      study_versions.each do |study_version|
+        study_version.name = @study.name
+        study_version.protocol = @study.protocol
+        study_version.agent = @study.agent
+        study_version.mechanism = @study.mechanism
+        study_version.side_effects = @study.side_effects
+        study_version.administration = @study.administration
+        study_version.randomization = @study.randomization
+        study_version.duration = @study.duration
+        study_version.assessment_frequency = @study.assessment_frequency
+        study_version.interventions = @study.interventions
+        study_version.sponsor_name = @study.sponsor_name
+        study_version.sponsor_contact = @study.sponsor_contact
+        study_version.cro_contact = @study.cro_contact
+        study_version.budget = @study.budget
+        study_version.comments = @study.comments
+        study_version.inclusion_criteria = @study.inclusion_criteria
+        study_version.exclusion_criteria = @study.exclusion_criteria
+        study_version.travel_parking_costs = @study.travel_parking_costs
+        study_version.sponsor_contact_email = @study.sponsor_contact_email
+        study_version.sponsor_contact_phone = @study.sponsor_contact_phone
+        study_version.cro_name = @study.cro_name
+        study_version.cro_contact_email = @study.cro_contact_email
+        study_version.cro_contact_phone = @study.cro_contact_phone
+        study_version.save
+      end
+    end
   end
 
   def study_params
