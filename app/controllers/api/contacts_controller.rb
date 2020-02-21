a9f9f4c75c4a9a363da2b269dd19be9ed9b6afcc
Chetan <cmchetanm@gmail.com>
2020-02-21 17:52:27 +0530

    fix issue related to user's studies

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -17,28 +17,36 @@ module Api
     end
 
     def create
-      @contact = Contact.new(contact_params.merge(creator: current_api_user))
-      study = Study.find_by(id: params[:study_id]) || StudyVersion.find_by(id: params[:study_id])
-      puts 'in create'
-      puts params
-      generated_password = rand.to_s[2..7]
-      puts 'generated_password'
-      puts generated_password
-      user = User.new({
-        :first_name => params["first_name"],
-        :last_name => params["last_name"],
-        :email => params["email"],
-        :password => generated_password,
-        :phone_number => params["phone_number"]
-      }.reject {|p| !p.present?})
-      user.skip_confirmation!
-      user.save!
-      BarsMailer.welcome_email(user, generated_password, study, :app, current_api_user&.full_name).deliver_now
+      user = User.find_by_email(params["email"])
+      unless user
+        @contact = Contact.new(contact_params.merge(creator: current_api_user))
+        study = Study.find_by(id: params[:study_id]) || StudyVersion.find_by(id: params[:study_id])
+        site_id = study.site_for(current_api_user)
+        role = params["role"]
+        puts 'in create'
+        puts params
+        generated_password = rand.to_s[2..7]
+        puts 'generated_password'
+        puts generated_password
+        user = User.new({
+          :first_name => params["first_name"],
+          :last_name => params["last_name"],
+          :email => params["email"],
+          :password => generated_password,
+          :phone_number => params["phone_number"]
+        }.reject {|p| !p.present?})
+        user.skip_confirmation!
+        user.save!
+        BarsMailer.welcome_email(user, generated_password, study, :app, current_api_user&.full_name).deliver_now
 
-      if @contact.save
-        render :show, status: :created
+        if @contact.save
+          create_share(site_id,study,user,role)
+          render :show, status: :created
+        else
+          render json: {errors: @contact.errors.full_messages}, status: :unprocessable_entity
+        end
       else
-        render json: {errors: @contact.errors.full_messages}, status: :unprocessable_entity
+        render json: {errors: "User with email address already exists"}, status: :unprocessable_entity
       end
     end
 
@@ -59,6 +67,10 @@ module Api
     end
 
     private
+
+    def create_share(site_id,study,user,role)
+      share = Share.create!(site_id: site_id, study_id: study.id, user_id: user.id, role: role)
+    end
 
     def set_contact
       @contact = Contact.find_by!(id: params[:id], creator: current_api_user)
